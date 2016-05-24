8029ddb0acbe488333aa399e5e7717fc878e1b33
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-24 15:01:48 -0500

    Add sass linting and eslint config

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,36 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const jshint = require("gulp-jshint");
+const sassLint = require("gulp-sass-lint");
+
+const sassPath = "./static/**/*.s+(a|c)ss";
 
 
-gulp.task("default", ["sass"]);
+gulp.task("default", ["js:lint", "sass"]);
+
+gulp.task("sass", ["sass:lint", "sass:compile", "sass:watch"]);
+gulp.task("lint", ["sass:lint", "js:lint"]);
 
 
-gulp.task("sass",["sass:compile", "sass:watch"]);
 gulp.task("sass:compile", () => (
-  gulp.src("./static/**/*.scss")
+  gulp.src(sassPath)
     .pipe(sass())
     .pipe(gulp.dest("./static/"))
 ));
+
 gulp.task("sass:watch", () =>
-  gulp.watch("./static/**/*.scss", ["sass"]
+  gulp.watch(sassPath, ["sass:lint", "sass:compile"]
+));
+
+gulp.task("sass:lint", () => (
+  gulp.src(sassPath)
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
 ));
 
 
-gulp.task("lint:js", () => (
+gulp.task("js:lint", () => (
   gulp.src("./static/**/*.js")
     .pipe(jshint())
     .pipe(jshint.reporter("jshint-stylish"))
